13a60f5aa3df99c3a1a5aaa6beddfc7f4bf943f0
Vorshim92 <vorshim92@gmail.com>
2024-02-07 19:35:39 +0100

    extra star hover

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -1,25 +1,36 @@
 const starsRating = () => {
   const divStar = document.querySelector(".stars");
   for (let i = 0; i < 10; i++) {
-    divStar.innerHTML += `<svg class="star" viewBox="0 0 47 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+    divStar.innerHTML += `<div class="div-star"><svg class="star" viewBox="0 0 47 46" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
           d="M22.2044 1.55551C22.6143 0.569963 24.0104 0.569964 24.4203 1.55552L29.9874 14.9402C30.1602 15.3557 30.5509 15.6396 30.9994 15.6756L45.4494 16.834C46.5134 16.9193 46.9448 18.2471 46.1341 18.9415L35.1248 28.3722C34.7831 28.6649 34.6338 29.1242 34.7382 29.5619L38.1018 43.6626C38.3494 44.7009 37.2199 45.5215 36.309 44.9651L23.9379 37.4089C23.5538 37.1743 23.0709 37.1743 22.6868 37.4089L10.3157 44.9651C9.40478 45.5215 8.27528 44.7009 8.52295 43.6626L11.8865 29.5619C11.9909 29.1242 11.8416 28.6649 11.4999 28.3722L0.490575 18.9415C-0.320069 18.2471 0.111362 16.9193 1.17535 16.834L15.6253 15.6756C16.0738 15.6396 16.4645 15.3557 16.6374 14.9402L22.2044 1.55551Z"
           fill="#080c29"
         />
-      </svg>`;
+      </svg></div>`;
   }
   clickStar();
 };
 
 const clickStar = () => {
-  const allStar = document.querySelectorAll(".star");
+  const allStar = document.querySelectorAll(".div-star");
+
   allStar.forEach((star, index) => {
+    star.addEventListener("mouseenter", function (e) {
+      for (let i = index - 1; i < allStar.length; i--) {
+        allStar[i].querySelector("svg > path").setAttribute("fill", "#395c5c");
+      }
+    });
+    star.addEventListener("mouseleave", function (e) {
+      for (let i = index - 1; i < allStar.length; i--) {
+        allStar[i].querySelector("svg > path").setAttribute("fill", "#080c29");
+      }
+    });
     star.addEventListener("click", function (e) {
       let rating = 0;
       for (let i = 0; i <= index; i++) {
-        allStar[i].querySelector("path").setAttribute("fill", "#00FFFF");
+        allStar[i].querySelector("svg > path").setAttribute("fill", "#00FFFF");
         for (let j = index + 1; j < allStar.length; j++) {
-          allStar[j].querySelector("path").setAttribute("fill", "#080c29");
+          allStar[j].querySelector("svg > path").setAttribute("fill", "#080c29");
         }
       }
 
