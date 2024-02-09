9a6048691e0d278acbf4c3562df2e76b0326262c
Vorshim92 <vorshim92@gmail.com>
2024-02-09 10:43:49 +0100

    feedback result

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -54,14 +54,69 @@ const submitRating = () => {
 
     feedback.push(obj);
 
-    window.location.href = "../../index.html";
+    feedbackResult(rating);
   });
 };
 
-const feedbackResult = () => {
+const feedbackResult = (feedbackSubmitted) => {
   const body = document.body;
   body.innerHTML = "";
-  body.innerHTML = "";
+  body.innerHTML = `<header>
+  <div class="logo"><img src="./assets/img/epicode_logo.png" alt="Epicode logo" /></div>
+</header>
+<main>
+  <div id="fb-container">
+    
+    
+  </div>
+</main>
+<footer>
+  <a href="https://epicode.com/"><button id="more">MORE INFO</button></a>
+  <p>&copy; EPICODE School</p>
+</footer>
+<!-- BEGIN_SCRIPT -->
+
+<script src="./assets/js/feedback.js"></script>
+<!-- END_SCRIPT -->`;
+  const containerFb = document.getElementById("fb-container");
+
+  if (feedbackSubmitted >= 6) {
+    containerFb.innerHTML = `<div id="positive-fb">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="30"
+      height="30"
+      fill="currentColor"
+      class="bi bi-emoji-smile"
+      viewBox="0 0 16 16"
+    >
+      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+      <path
+        d="M4.285 9.567a.5.5 0 0 1 .683.183A3.5 3.5 0 0 0 8 11.5a3.5 3.5 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5m4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5"
+      />
+    </svg>
+    <p>Siamo felici che la tua esperienza sia stata positiva e non vediamo l'ora di rivederti.</p>
+  </div>`;
+  } else
+    containerFb.innerHTML = `<div id="negative-fb">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="30"
+    height="30"
+    fill="currentColor"
+    class="bi bi-emoji-frown"
+    viewBox="0 0 16 16"
+  >
+    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+    <path
+      d="M4.285 12.433a.5.5 0 0 0 .683-.183A3.5 3.5 0 0 1 8 10.5c1.295 0 2.426.703 3.032 1.75a.5.5 0 0 0 .866-.5A4.5 4.5 0 0 0 8 9.5a4.5 4.5 0 0 0-3.898 2.25.5.5 0 0 0 .183.683M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5m4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5"
+    />
+  </svg>
+  <p>
+    Ci dispiace che la tua esperienza non sia stata del tutto positiva! Non vediamo comunque l'ora di rivederti
+    per farti cambiare idea.
+  </p>
+</div>`;
 };
 
 window.onload = function () {
